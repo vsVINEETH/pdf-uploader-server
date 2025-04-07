a8f604907f21e2ffb167ad0a952eb627c3e1228c
Vineeth <vineethvs1927@gmail.com>
2025-04-07 14:56:35 +0530

    cookie config updated

--- src/config/cookieOptions.ts
+++ src/config/cookieOptions.ts
@@ -1,5 +1,11 @@
+// export const setCookieOptions = {
+//     httpOnly: true,
+//     secure: false,
+//     sameSite: 'strict' as const
+// };
+
 export const setCookieOptions = {
     httpOnly: true,
-    secure: false,
-    sameSite: 'strict' as const
+    secure: true, // Must be true in production for HTTPS
+    sameSite: 'none' as const // Allows cross-site cookies
 };
